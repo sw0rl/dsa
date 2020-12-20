7238d9f0e06cdd56e4c36cbdb8a4b9a5813845e5
Caleb A <73247137+sw0rl@users.noreply.github.com>
2020-12-20 20:57:45 +0000

    Added TwoNumberSum.java

---
+++ TwoNumberSum.java
@@ -0,0 +1,23 @@
+import java.util.HashSet;
+
+public class TwoNumberSum {
+	public static void main(String[] args) {
+		int[] arr = {3, 5, -4, 8, 11, 1, -1, 6};
+		HashSet<Integer> integers = new HashSet<>();
+		for (int i = 0; i < arr.length; i++) {
+			integers.add(arr[i]);
+		}
+		
+		int targetSum = 10;
+		
+		for (int j = 0; j < arr.length; j++) {
+			int validInt = targetSum - arr[j];
+			if (integers.contains(validInt)) {
+				System.out.println("There is a pair contained.");
+				break;
+			} else {
+				System.out.println("There is no pair.");
+			}
+		}
+	}
+}
